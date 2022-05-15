1702a6134f71034ab7afd4fdcc653b840a072cff
ritika-rm <60855163+ritika-rm@users.noreply.github.com>
2022-05-15 16:50:30 +0530

    Update find-the-duplicate-number.cpp

--- Day1/find-the-duplicate-number.cpp
+++ Day1/find-the-duplicate-number.cpp
@@ -31,3 +31,21 @@ public:
         return x;
     }      
 };
+
+
+OR
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+            if(mp[nums[i]]==2)
+                return nums[i]; 
+        }
+       
+        return -1;
+    }      
+};
