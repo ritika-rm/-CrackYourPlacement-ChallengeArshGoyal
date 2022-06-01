4127d2ef24a7d7216c76471585a57f1ab9d00b4a
ritika-rm <60855163+ritika-rm@users.noreply.github.com>
2022-06-01 19:12:02 +0530

    Update find-all-duplicates-in-an-array.cpp

--- Day 9/find-all-duplicates-in-an-array.cpp
+++ Day 9/find-all-duplicates-in-an-array.cpp
@@ -18,3 +18,20 @@ public:
 
 omw
  earlier i had attempted this question but i thought there hasto be some special way to return vector lol. it was this easy.
+
+    
+    vector<int> findDuplicates(vector<int>& nums) {
+		int a[nums.size()+1];
+			memset(a,-1,sizeof a);
+			for(int i=0;i<nums.size();i++){
+				a[nums[i]]++;
+			}
+			vector<int>ans;
+			for(int i=1;i<=nums.size();i++){
+				if(a[i]==1){
+					ans.push_back(i);
+				}
+			}
+			return ans;
+			}
+from discussion, using memset to assign -1 to all, and then add frequency by traversing nums array. faster soln
