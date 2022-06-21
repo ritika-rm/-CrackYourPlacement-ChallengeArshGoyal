1e8d06ae5870072620c529898254b1f6c65e7b5a
ritika-rm <60855163+ritika-rm@users.noreply.github.com>
2022-06-21 21:45:51 +0530

    Update two-sum.cpp

--- Day 3/two-sum.cpp
+++ Day 3/two-sum.cpp
@@ -26,3 +26,23 @@ public:
     }
 };
 omw
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        unordered_map<int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(target - nums[i]) != mpp.end()){
+                ans.push_back(mpp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            mpp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
+
+using hashmap, brilliant approach, storing number and index while traversing, to find it and return it later
