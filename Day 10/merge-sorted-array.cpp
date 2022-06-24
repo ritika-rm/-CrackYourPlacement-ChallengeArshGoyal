937bec1c8d096b67758b089d2fc00b570b7b8550
ritika-rm <60855163+ritika-rm@users.noreply.github.com>
2022-06-24 20:37:44 +0530

    Update merge-sorted-array.cpp

--- Day 10/merge-sorted-array.cpp
+++ Day 10/merge-sorted-array.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j=n-1;
+        int j=0;
         for(int i=m+n-1;i>=m;i--)
         {
             if(nums1[i]==0)
                 {swap(nums1[i],nums2[j]);
-                j--;}
+                j++;}
             
         }
         sort(nums1.begin(),nums1.end());
     }
 };
 entirely omown lol. i first did a silly mistake of traversing from the 0th index, which obviously showed tle, then i started form the last index and boom.
+    O((m+n)log(m+n) time complexity
+    
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1, j = n-1, TotalArrangements =m+n-1;
+        while(j>=0){
+            nums1[TotalArrangements--] = i>=0 && nums1[i]>nums2[j] ?nums1[i--]:nums2[j--];
+        }
+    }
+};
+      
+      two pointer. O(m+n) TC.
