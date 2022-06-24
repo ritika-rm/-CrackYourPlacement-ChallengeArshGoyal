d74292eba3624541bc641989fdb5df1824e7faa0
ritika-rm <60855163+ritika-rm@users.noreply.github.com>
2022-06-25 05:10:34 +0530

    Create a-product-array-puzzle.cpp

---
+++ Day 15/a-product-array-puzzle.cpp
@@ -0,0 +1,7 @@
+https://www.geeksforgeeks.org/a-product-array-puzzle/
+
+
+my approach
+product of entire array, then replace each element by dividing the ith element from the product.
+  
+  other approaches are given and for what?
